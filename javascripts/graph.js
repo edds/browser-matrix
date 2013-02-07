8154756ed713c34cb9a713d2fabb42cf8e01763d
Edd Sowden <edd@mintdigital.com>
2013-02-07 19:45:19 +0000

    Labeled graphs

--- javascripts/graph.js
+++ javascripts/graph.js
@@ -10,19 +10,20 @@
     y: false,
     width: 600,
     height: 300,
+    maxPercent: 20,
 
     init: function(){
-      var padding = 20,
+      var padding = 40,
           axis, axisCords;
 
       graph.svg = d3.select('#graph')
         .append('svg:svg')
-          .attr('width', (graph.width + padding * 2))
-          .attr('height', (graph.height + padding * 2))
+          .attr('width', (graph.width + 300 + padding * 2))
+          .attr('height', (graph.height + 60 + padding * 2))
           .attr('class', 'multiLine')
         .append('g')
           .attr('transform', 'translate('+padding+','+padding+')'),
-      axis = graph.svg.append('g').attr('class', 'axis'),
+      graph.axis = graph.svg.append('g').attr('class', 'axis'),
       axisCords = [
         // [ y1, y2, x1, x2 ]
         [ 0.5, graph.width+.5, graph.height+.5, graph.height+.5 ],
@@ -30,15 +31,13 @@
         [ graph.width+.5, graph.width+.5, 0.5, graph.height+.5 ]
       ];
 
-      graph.lines = graph.svg.append('g').attr('class', 'lines')
+      graph.browsers = graph.svg.append('g').attr('class', 'browsers')
       graph.colours = d3.scale.category10();
 
       axisCords.map(function(d){
-        axis.append('svg:line')
+        graph.axis.append('svg:line')
           .attr('x1', d[0]).attr('x2', d[1]).attr('y1', d[2]).attr('y2', d[3]);
       });
-
-      graph.y = d3.scale.linear().domain([0, 22]).range([graph.height, 0]).nice();
 
       graph.line = d3.svg.line()
         .interpolate('basis')
@@ -46,21 +45,83 @@
         .y(function(d, i) { return graph.y(d); });
     },
     addData: function(days, data){
-      var i, _i, line;
+      var browser, browsers, tick, ticks;
+      if(data[0].days[data[0].days.length-1] > graph.maxPercent){
+        graph.maxPercent = data[0].days[data[0].days.length - 1];
+      }
       graph.x = d3.scale.linear().domain([0, days.length-1]).range([graph.width, 0]);
+      graph.y = d3.scale.linear().domain([0, graph.maxPercent]).range([graph.height, 0]).nice();
 
-      line = graph.lines.selectAll('.line')
-      .data(data.slice(0,15));
+      // add new data to the browsers
+      browsers = graph.browsers.selectAll('.browser')
+        .data(data.slice(0,15), function(d){ return d.os+'-'+d.browser+'-'+d.version; })
 
-      line.transition()
-            .attr('d', function(d){ return graph.line(d.days); });
+      // add new elemnts not matched by previous data
+      browser = browsers.enter()
+        .append('g').attr('class', 'browser')
 
-      
-      line.enter()
-          .append('path')
-            .attr('class', 'line')
-            .attr('d', function(d){ return graph.line(d.days); })
-            .attr('stroke', function(d,i){ return graph.colours(i); });
+      // add new text nodes
+      browser.append("text")
+        .datum(function(d) { return {name: d.os+' -  '+d.browser+' - '+d.version, value: d.days[0]}; })
+        .attr("transform", function(d){ return "translate("+graph.width+","+graph.y(d.value)+")"; })
+        .attr("x", 3)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+
+      // add new lines
+      browser.append('path')
+        .attr('class', 'line')
+        .attr('stroke', function(d,i){ return graph.colours(i); });
+
+      // update all the lines to have the new data
+      browsers.select('.line').transition()
+        .attr('d', function(d){ return graph.line(d.days); });
+
+      // This uses a nasty array hack to get the dates reversed without effecting
+      // the original
+      ticks = graph.axis.selectAll('.x-tick')
+        .data(Array.apply(null,days).reverse())
+
+      tick = ticks.enter()
+        .append('g').attr('class', 'x-tick')
+      tick.append('svg:line')
+        .attr('class', 'line')
+        .attr('y1', 0)
+        .attr('y2', graph.height);
+      tick.append('text')
+        .attr('class', 'text')
+        .attr('text-anchor', 'start')
+        .attr("dy", '15px')
+        .attr('transform', function(d, i) { return 'translate(0,'+graph.height+') rotate(45)'; })
+        .text(function(d){ return d; });
+
+      ticks.select('.line').transition()
+        .attr('x1', function(d, i){ return graph.x(i)+.5 })
+        .attr('x2', function(d, i){ return graph.x(i)+.5 });
+      ticks.select('.text').transition()
+        .attr('transform', function(d, i) { return 'translate('+graph.x(i)+','+graph.height+') rotate(45)'; });
+
+      ticks = graph.axis.selectAll('.y-tick')
+        .data(graph.y.ticks(5))
+
+      tick = ticks.enter()
+        .append('g').attr('class', 'y-tick')
+      tick.append('svg:line')
+        .attr('class', 'line')
+        .attr('x1', 0)
+        .attr('x2', graph.width);
+      tick.append('text')
+        .attr('class', 'text')
+        .attr('text-anchor', 'end')
+        .attr('dy', '.4em')
+        .attr('transform', function(d, i) { return 'translate(-2,0)'; })
+        .text(function(d){ return d +'%'; });
+
+      ticks.select('.line').transition()
+        .attr('y1', function(d, i){ return graph.y(d)+.5 })
+        .attr('y2', function(d, i){ return graph.y(d)+.5 });
+      ticks.select('.text').transition()
+        .attr('transform', function(d, i) { return 'translate(-2, '+graph.y(d)+')'; });
     }
   };
   root.matrix.graph = graph;
