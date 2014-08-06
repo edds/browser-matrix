088c8d0a8dc4c36eb287ce0f4a046fcca3ec397f
Edd Sowden <e@e26.co.uk>
2014-08-06 08:17:09 +0000

    Clear cache after 24 hours
    Clear the cache after 24 hours. It could be stale by then anyway.

--- javascripts/cache.js
+++ javascripts/cache.js
@@ -6,8 +6,12 @@
 
   var cache = function(attr, value){
     if (typeof root.localStorage === 'undefined'){
-      root.localStorage = {};
+      root.localStorage = {
+        clear: function(){} // no-op, it will be empty on page refresh anyway
+      };
     }
+
+    clearOldCaches();
 
     if(typeof value === 'undefined'){
       if(typeof root.localStorage[attr] !== 'undefined'){
@@ -25,6 +29,27 @@
       }
     }
   };
+
+  var clearOldCaches = function(){
+    var timeSet = root.localStorage.cacheTime,
+        cacheTimeout = 60 * 60 * 24,
+        cacheAgeInSeconds;
+
+    if(timeSet){
+      timeSet = root.parseInt(timeSet, 10);
+      cacheAgeInSeconds = (Date.now() - timeSet) / 1e3;
+      if(cacheAgeInSeconds > cacheTimeout){
+        localStorage.clear();
+        root.localStorage.cacheTime = Date.now();
+      }
+    } else if(root.localStorage.length){
+      // legacy data in the storage, clear it.
+      localStorage.clear();
+    } else {
+      root.localStorage.cacheTime = Date.now();
+    }
+  };
+
   root.matrix.cache = cache;
 
 }).call(this);
